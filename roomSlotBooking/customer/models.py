490be1494978992234281e0653a3dbb92d49327c
priyam <priyamshankar.5@gmail.com>
2022-04-15 21:52:15 +0530

    make the first migrations

--- roomSlotBooking/customer/models.py
+++ roomSlotBooking/customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from sqlalchemy import true
 
 # Create your models here.
+
+class Customers(models.Model):
+    name=models.CharField(max_length=200,null=true)
+    userId=models.CharField(max_length=200,null=true)
+    password=models.CharField(max_length=50,null=true)
+
